c729aaffc7f990cf068c830b89d561874d912aff
vantran445 <CaoVan.Info@gmail.com>
2023-05-23 22:16:32 +0700

    Update AbstractTerm.php

--- src/Terms/AbstractTerm.php
+++ src/Terms/AbstractTerm.php
@@ -1,75 +1,22 @@
 <?php namespace VanTran\NhamBaseTerms\Terms;
 
-abstract class AbstractTerm
+use VanTran\NhamBaseTerms\Contracts\TermInterface;
+use VanTran\NhamBaseTerms\Traits\HasAliasTrait;
+use VanTran\NhamBaseTerms\Traits\HasIndexTrait;
+use VanTran\NhamBaseTerms\Traits\HasKeyTrait;
+
+abstract class AbstractTerm implements TermInterface
 {
-    private $associated = [];
+    use HasIndexTrait;
+    use HasKeyTrait;
+    use HasAliasTrait;
 
     public function __construct(
         public readonly int $index,
         public readonly string $key,
-        public readonly int $element,
-        public readonly int $yinyang,
         protected array $alias = []
     )
     {
         
-    }
-
-    public function __get($name)
-    {
-        return $this->getAttribute($name);
-    }
-
-    public function __set(string $name, mixed $value)
-    {
-        $this->setAttribute($name, $value);
-    }
-
-    public function getIndex(): int
-    {
-        return $this->index;
-    }
-
-    public function getKey(): string
-    {
-        return $this->key;
-    }
-
-    public function getElement(): int
-    {
-        return $this->element;
-    }
-
-    public function getYinYang(): int
-    {
-        return $this->yinyang;
-    }
-
-    public function getAlias(): array
-    {
-        return $this->alias;
-    }
-
-    public function setAlias(string $alias): self
-    {
-        array_push($this->alias, $alias);
-        return $this;
-    }
-
-    public function getAttribute(string $key): mixed
-    {
-        if (!isset($this->associated[$key])) {
-            return null;
-        }
-
-        return $this->associated[$key];
-    }
-
-    public function setAttribute(string $key, mixed $associate): self
-    {
-        $val = (is_callable($associate)) ? $associate() : $associate;
-        $this->associated[$key] = $val;
-
-        return $this;
     }
 }
