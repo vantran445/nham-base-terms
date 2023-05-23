2a068a8c1bf9b79c7d5b46197023a3bde6161df9
vantran445 <CaoVan.Info@gmail.com>
2023-05-23 21:54:42 +0700

    Create ElementTest.php

---
+++ tests/ElementTest.php
@@ -0,0 +1,39 @@
+<?php namespace VanTran\NhamBaseTerms\Tests;
+
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
+use Throwable;
+use VanTran\NhamBaseTerms\Element;
+
+class ElementTest extends TestCase
+{
+    /**
+     * @covers Element
+     * 
+     * @return void 
+     * @throws Throwable 
+     * @throws ExpectationFailedException 
+     */
+    public function testResolvingNormalWay(): void
+    {
+        $term = Element::term('water');
+
+        $this->assertEquals(5, $term->getIndex());
+        $this->assertTrue($term === Element::term('e'));
+    }
+
+    /**
+     * @covers Element
+     * 
+     * @return void 
+     * @throws ExpectationFailedException 
+     */
+    public function testResolvingMagicWay(): void
+    {
+        $term = Element::kim();
+
+        $this->assertEquals(4, $term->getIndex());
+        $this->assertTrue($term === Element::metal());
+        $this->assertTrue($term === Element::d());
+    }
+}
